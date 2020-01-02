2998754bbf7ebecedc000528baea09a5c1837b45
Guilherme França <guii_real@hotmail.com>
2020-01-01 23:42:35 -0300

    Promises

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-var xhr = new XMLHttpRequest();
+var myPromise = function () {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET','https://api.github.com/users/guiireal');
+    xhr.send(null);
 
-xhr.open('GET', 'https://api.github.com/users/guiireal');
-xhr.send(null);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        resolve(JSON.parse(xhr.responseText));
+      }
+    }
 
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4) {
-    console.log(JSON.parse(xhr.responseText));
-  }
+  })
 };
+
+myPromise().then(response => console.log(response));
